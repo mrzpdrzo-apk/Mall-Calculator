6478aa82942d6226c98c8f9d745a0bb96af0ec1f
mer💜🎀 <meriechristpedroso@gmail.com>
2026-02-23 21:58:38 +0800

    Add files via upload

---
+++ update mall calculator.py
@@ -0,0 +1,95 @@
+products = []
+prices = []
+quantities = []
+total_amount  = 0
+changes = 0
+choice = ""
+print("^_^------------Mall Caculator---------^_^\n\n\n")
+
+while True:
+    product = input("Enter Product: ")
+    price = float(input("Enter Price: "))
+    
+    while True:
+        quantity = int(input("Enter Quantity: ")) 
+        if quantity < 0:
+            print("Quantity cannot be negative!")
+        else:
+            break    
+           
+    while True:        
+        choice = input("\nWould you like to add  another product? Type Y for yes Q to quit: ")
+        print()
+        
+        if choice.lower()== "y":
+             products.append(product)
+             prices.append(price) 
+             quantities.append(quantity)
+             break
+             
+        elif choice.lower() == "q":
+             break
+        
+        else:
+            print("Type Q and Y only!")
+        
+        
+           
+
+print("---------------------------------------------\n") 
+print("Food/Product you order:\n ")   
+
+for productName in products:
+      print(productName)
+
+for i in range(len(prices)):
+      total_amount += prices[i] * quantities[i] 
+
+
+
+print("---------------------------------------------\n")
+
+amount = float(input("\n\nEnter Amount: "))
+
+print(f"Total: {total_amount:.2f}")
+
+if (amount >= total_amount) :
+    changes = (amount - total_amount)
+    print(f"Change: {changes: .2f}\n\n")
+
+
+
+    print("---------------------------------------------\n")
+    print("Reciept\n")
+    print("---------------------------------------------\n")
+
+    print(f"Total: {total_amount:.2f}")
+    print(f"Amount: {amount:.2f}")
+    print(f"Change: {changes:.2f}\n")    
+
+
+    print("---------------------------------------------\n")
+    print("Ordered Food\n")
+    print("---------------------------------------------\n")
+
+    print(f" {'Food':<15} | {'Quantity' :^10} | {'Price':>8}")
+
+    for i in range(len(products)):
+      print(f" {products[i] :<15} | {quantities[i]:^10} | {prices[i]:>8.2f}")
+
+    print("---------------------------------------------\n\n")
+
+    print("^_^ Comeback Again\n\n")
+
+    print("---------------------------------------------\n\n")
+
+
+else:
+    print("Insufficient Money\n")
+
+    print("---------------------------------------------\n\n")
+
+    print("^_^ Comeback Again\n\n")
+
+    print("---------------------------------------------\n\n")
+    
